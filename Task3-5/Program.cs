a7919805f027fe545af524f0b7e1efb70a62db47
the_nza <the_nza@protonmail.com>
2021-09-27 15:19:36 +0300

    Complete Task3-5

---
+++ Task3-5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task3_5
+{
+	static class Swapper
+	{
+		public static void Swap(ref int a, ref int b)
+		{
+			(b, a) = (a, b); // Обмен через деконструкцию кортежа
+		}
+	}
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 33;
+			int b = 98;
+			Console.WriteLine("a is {0}, b is {1}", a, b);
+			
+			Swapper.Swap(ref a, ref b);	
+			Console.WriteLine("a is {0}, b is {1}", a, b);
+		}
+	}
+}
