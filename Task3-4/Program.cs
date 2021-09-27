8d7ac4c2f526bc6428177a2822231bbd0e12eb95
the_nza <the_nza@protonmail.com>
2021-09-27 15:15:01 +0300

    Complete Task3-4

---
+++ Task3-4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task3_4
+{
+	static class Ops
+	{
+		public static int Sum(params int[] arr)
+		{
+			int sum = 0;
+
+			foreach (int v in arr)
+			{
+				sum += v;
+			}
+
+			return sum;
+		}
+	}
+	
+	class Program
+	{
+		
+		static void Main()
+		{
+			int sum = Ops.Sum(1, 2, 3, 4, 5);
+			Console.WriteLine("Sum of 1,2,3,4,5 = {0}" , sum);
+		}
+	}
+}
